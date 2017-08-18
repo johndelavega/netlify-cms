bbecdf79613868ec5aa2ddd3f7316d462c32c909
Shawn Erquhart <shawn@erquh.art>
2017-08-18 13:13:40 -0400

    support media library in list and object widgets

--- src/components/Widgets/ControlHOC.js
+++ src/components/Widgets/ControlHOC.js
@@ -14,6 +14,7 @@ class ControlHOC extends Component {
       PropTypes.string,
       PropTypes.bool,
     ]),
+    mediaPaths: ImmutablePropTypes.map.isRequired,
     metadata: ImmutablePropTypes.map,
     onChange: PropTypes.func.isRequired,
     onValidate: PropTypes.func.isRequired,
@@ -90,6 +91,7 @@ class ControlHOC extends Component {
       controlComponent,
       field,
       value,
+      mediaPaths,
       metadata,
       onChange,
       onOpenMediaLibrary,
@@ -100,6 +102,7 @@ class ControlHOC extends Component {
     return React.createElement(controlComponent, {
       field,
       value,
+      mediaPaths,
       metadata,
       onChange,
       onOpenMediaLibrary,
